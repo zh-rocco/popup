6b1cbc7eeffc0c226217a0586d5bf07aa13590de
no-nothing <pzzd0310@163.com>
2017-01-22 16:31:30 +0800

    精简show()函数，添加timeout参数

--- src/js/popup.js
+++ src/js/popup.js
@@ -33,7 +33,8 @@ function popup() {
 
     var popupBox = document.getElementById('popup-box');
     var popupMask = popupBox.getElementsByClassName('popup-mask')[0];
-    var popupItems = []; //popupBox下的所有弹窗集合
+    var popupItems = []; //popupBox下的所有已使用的弹窗集合
+    var popupType = ['confirm', 'loading', 'success', 'failure']; //目前支持的弹窗类型
     var options = {
         confirm: {
             width: '',
@@ -44,18 +45,21 @@ function popup() {
                 var popupConfirm = popupBox.getElementsByClassName('popup-confirm')[0];
                 popupBox.style.display = 'none';
                 popupConfirm.style.display = 'none';
+                console.log('success');
             },
             failure: function () {
                 var popupConfirm = popupBox.getElementsByClassName('popup-confirm')[0];
                 popupBox.style.display = 'none';
                 popupConfirm.style.display = 'none';
+                console.log('failure');
             }
         },
         loading: {
             width: '',
             Height: '',
             maskColor: 'transparent',
-            text: '解绑中...'
+            text: '解绑中...',
+            timeout: '2000'
         },
         success: {
             width: '',
@@ -87,104 +91,66 @@ function popup() {
     popupBox.addEventListener('click', stop, false);
 
     //展示指定的popupItem，传入参数 confirm:确认? loading:加载动画 success:成功 failure:失败
-    function show(arg, opt) {
+    function show(type, opt) {
+        //若不支持报错，停止运行
+        if (popupType.indexOf(type) === -1) {
+            console.error('请输入合法的"show(arg)",arg可选"confirm" "success" "failure" "loading"');
+            return false;
+        }
+
+        //未传入opt的时候opt={}
         opt = opt || {};
 
-        switch (arg) {
-            case 'confirm':
-                showConfirm(opt);
-                break;
-            case 'loading':
-                showLoading(opt);
-                break;
-            case 'success':
-                showSuccess(opt);
-                break;
-            case 'failure':
-                showFailure(opt);
-                break;
-            default:
-                console.error('请输入合法的"show(arg)",arg可选"confirm" "success" "failure" "loading"');
-                return false;
+        //若DOM无相应节点，创建
+        if (!popupBox.getElementsByClassName('popup-' + type)[0]) {
+            create(type);
         }
-    }
 
-    /* 确认弹窗 */
-    function showConfirm(opt) {
-        if (!popupBox.getElementsByClassName('popup-confirm')[0]) {
-            create('confirm');
+        //重置默认内容
+        if (opt.text) {
+            options[type].text = opt.text;
         }
-        popupMask.style.backgroundColor = opt.maskColor || options.confirm.maskColor;
+        if (opt.maskColor) {
+            options[type].maskColor = opt.maskColor;
+        }
+
+        //配置样式和内容等
+        popupMask.style.backgroundColor = options[type].maskColor;
         close();
-        var popupConfirm = popupBox.getElementsByClassName('popup-confirm')[0];
-        var text = popupConfirm.getElementsByClassName('text')[0];
-        var yes = popupConfirm.getElementsByClassName('yes')[0];
-        var no = popupConfirm.getElementsByClassName('no')[0];
-        text.textContent = opt.text || options.confirm.text;
-        popupConfirm.style.display = 'block';
+        var popupItem = popupBox.getElementsByClassName('popup-' + type)[0];
+        var text = popupItem.getElementsByClassName('text')[0];
+        text.textContent = options[type].text;
+
+        //confirm添加点击事件
+        if (type === 'confirm') {
+            var yes = popupItem.getElementsByClassName('yes')[0];
+            var no = popupItem.getElementsByClassName('no')[0];
+            addEvent(yes, opt.success, options.confirm.success);
+            addEvent(no, opt.failure, options.confirm.failure);
+        }
+
+        popupItem.style.display = 'block';
         popupBox.style.display = 'block';
-        addEvent(yes, opt.success, options.confirm.success);
-        addEvent(no, opt.failure, options.confirm.failure);
+
+        if (opt.timeout) {
+            setTimeout(function () {
+                close();
+            }, opt.timeout)
+        }
     }
 
     //confirm弹窗添加回掉函数
     function addEvent(ele, cb, def) {
-        if (isFunction(cb)) {
-            ele.onclick = function () {
-                cb();
-                def();
-            }
-        } else {
+        isFunction(cb) ? ele.onclick = function () {
+            def();
+            cb();
+        } :
             ele.onclick = def;
-        }
-    }
-
-    /* 加载弹窗 */
-    function showLoading(opt) {
-        if (!popupBox.getElementsByClassName('popup-loading')[0]) {
-            create('loading');
-        }
-        popupMask.style.backgroundColor = opt.maskColor || options.loading.maskColor;
-        close();
-        var popupLoading = popupBox.getElementsByClassName('popup-loading')[0];
-        var text = popupLoading.getElementsByClassName('text')[0];
-        text.textContent = opt.text || options.loading.text;
-        popupLoading.style.display = 'block';
-        popupBox.style.display = 'block';
-    }
-
-    /* 成功弹窗 */
-    function showSuccess(opt) {
-        if (!popupBox.getElementsByClassName('popup-success')[0]) {
-            create('success');
-        }
-        popupMask.style.backgroundColor = opt.maskColor || options.loading.maskColor;
-        close();
-        var popupSuccess = popupBox.getElementsByClassName('popup-success')[0];
-        var text = popupSuccess.getElementsByClassName('text')[0];
-        text.textContent = opt.text || options.success.text;
-        popupSuccess.style.display = 'block';
-        popupBox.style.display = 'block';
-    }
-
-    /* 失败弹窗 */
-    function showFailure(opt) {
-        if (!popupBox.getElementsByClassName('popup-failure')[0]) {
-            create('failure');
-        }
-        popupMask.style.backgroundColor = opt.maskColor || options.loading.maskColor;
-        close();
-        var popupFailure = popupBox.getElementsByClassName('popup-failure')[0];
-        var text = popupFailure.getElementsByClassName('text')[0];
-        text.textContent = opt.text || options.failure.text;
-        popupFailure.style.display = 'block';
-        popupBox.style.display = 'block';
     }
 
     /* 关闭所有弹窗 */
     function close() {
         popupItems = popupBox.getElementsByClassName('popup-item');
-        //console.log(popupItems);
         popupBox.style.display = 'none';
         for (var i = 0, len = popupItems.length; i < len; i++) {
             popupItems[i].style.display = 'none';
@@ -193,22 +159,21 @@ function popup() {
 
     //生成HTML，type取 'confirm','loading','success','failure'
     function create(type) {
-        //var popupBox = document.getElementById('popup-box');
         var popupItem = document.createElement('div');
         popupItem.classList.add('popup-item', 'popup-' + type);
         popupItem.innerHTML = popupHTML[type];
         popupBox.appendChild(popupItem);
     }
 
-    //判断是否为一个函数
+    //判断是否为函数
     function isFunction(fn) {
         return Object.prototype.toString.call(fn) === '[object Function]';
     }
 
     //暴露API
     return {
-        show: function (arg, opt) {
-            show(arg, opt);
+        show: function (type, opt) {
+            show(type, opt);
         },
         close: function () {
             close();
